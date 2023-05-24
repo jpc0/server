2572142e3f2f6dd18c3a1f890560cc4dafa7fb2b
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2023-05-24 19:25:05 +0200

    Update to C++17

--- src/CMakeModules/Bootstrap_Linux.cmake
+++ src/CMakeModules/Bootstrap_Linux.cmake
@@ -79,7 +79,6 @@ endif()
 IF (NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
 	ADD_COMPILE_OPTIONS (-O3) # Needed for precompiled headers to work
 endif()
-ADD_COMPILE_OPTIONS (-std=c++14) # Needed for precompiled headers to work
 ADD_COMPILE_OPTIONS (-msse3)
 ADD_COMPILE_OPTIONS (-mssse3)
 ADD_COMPILE_OPTIONS (-msse4.1)
